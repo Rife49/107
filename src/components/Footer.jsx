115bbce61f9cc3cfb81a2b2d107f1d2c689af6e2
Rife49 <rife4949@yahoo.com>
2026-04-11 11:40:43 -0700

    version 2

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer(){
+
+    return(
+        <div>
+            <h4>This is my Footer</h4>
+        </div>
+    )
+}
+
+
+
+export default Footer;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css"
+
+function NavBar(){
+    return(
+        <h1>this is my NavBar</h1>
+    );
+} 
+
+
+
+export default NavBar;
